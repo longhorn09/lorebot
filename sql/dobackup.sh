276bb634e86cae2bf7cc3439e88354e03b6dfa16
Norman Tang <norman@normstorm.com>
2025-07-06 12:19:04 -0400

    7319 lores backup

--- sql/dobackup.sh
+++ sql/dobackup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysqldump --routines -u ntang -p --databases Lorebot > load_lorebot_db.sql
+mysqldump --routines -h 123.123.123.123 -P 3306 --set-gtid-purged=OFF --single-transaction -u ntang -p --databases Lorebot > load_lorebot_db.sql
 
